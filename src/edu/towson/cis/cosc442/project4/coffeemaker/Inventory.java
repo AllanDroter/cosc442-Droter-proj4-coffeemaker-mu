b61683ee500e72b825b69431cfd42daddd12f7f3
AllanDroter <allan.droter@gmail.com>
2024-04-12 15:47:23 -0400

    did a ton of changes that it suggested made it better and more object oriented

--- src/edu/towson/cis/cosc442/project4/coffeemaker/Inventory.java
+++ src/edu/towson/cis/cosc442/project4/coffeemaker/Inventory.java
@@ -2,132 +2,135 @@ package edu.towson.cis.cosc442.project4.coffeemaker;
 
 /**
  * Inventory for the coffee maker
+ * 
  * @author Josh
  * @version $Revision: 1.0 $
  */
 public class Inventory {
-    
-    private static int coffee;
-    private static int milk;
-    private static int sugar;
-    private static int chocolate;
-    
+
+    private int coffee;
+    private int milk;
+    private int sugar;
+    private int chocolate;
+
     public Inventory() {
-    	setCoffee(15);
-    	setMilk(15);
-    	setSugar(15);
-    	setChocolate(15);
+        coffee = 15;
+        milk = 15;
+        sugar = 15;
+        chocolate = 15;
     }
-    
+
     /**
      * Method getChocolate.
+     * 
      * @return int
      */
     public int getChocolate() {
         return chocolate;
     }
+
     /**
      * Method setChocolate.
+     * 
      * @param chocolate int
      */
     public void setChocolate(int chocolate) {
-    	if(chocolate >= 0) {
-    		Inventory.chocolate = chocolate;
-    	}
-    	else {
-    		Inventory.chocolate = 0;
-    	}
-        
+        if (chocolate >= 0) {
+            this.chocolate = chocolate;
+        } else {
+            this.chocolate = 0;
+        }
     }
+
     /**
      * Method getCoffee.
+     * 
      * @return int
      */
     public int getCoffee() {
         return coffee;
     }
+
     /**
      * Method setCoffee.
+     * 
      * @param coffee int
      */
     public void setCoffee(int coffee) {
-    	if(coffee >= 0) {
-    		Inventory.coffee = coffee;
-    	}
-    	else {
-    		Inventory.coffee = 0;
-    	}
+        if (coffee >= 0) {
+            this.coffee = coffee;
+        } else {
+            this.coffee = 0;
+        }
     }
+
     /**
      * Method getMilk.
+     * 
      * @return int
      */
     public int getMilk() {
         return milk;
     }
+
     /**
      * Method setMilk.
+     * 
      * @param milk int
      */
     public void setMilk(int milk) {
-    	if(milk >= 0) {
-    		Inventory.milk = milk;
-    	}
-    	else {
-    		Inventory.milk = 0;
-    	}
+        if (milk >= 0) {
+            this.milk = milk;
+        } else {
+            this.milk = 0;
+        }
     }
+
     /**
      * Method getSugar.
+     * 
      * @return int
      */
     public int getSugar() {
         return sugar;
     }
+
     /**
      * Method setSugar.
+     * 
      * @param sugar int
      */
     public void setSugar(int sugar) {
-    	if(sugar >= 0) {
-    		Inventory.sugar = sugar;
-    	}
-    	else {
-    		Inventory.sugar = 0;
-    	}
+        if (sugar >= 0) {
+            this.sugar = sugar;
+        } else {
+            this.sugar = 0;
+        }
     }
-    
+
     /**
      * Returns true if there are enough ingredients to make
      * the beverage.
+     * 
      * @param r
-    
-     * @return boolean */
+     * 
+     * @return boolean
+     */
     public boolean enoughIngredients(Recipe r) {
-        boolean isEnough = true;
-        if(Inventory.coffee < r.getAmtCoffee()) {
-            isEnough = false;
-        }
-        if(Inventory.milk < r.getAmtMilk()) {
-            isEnough = false;
-        }
-        if(Inventory.sugar < r.getAmtSugar()) {
-            isEnough = false;
-        }
-        if(Inventory.chocolate < r.getAmtChocolate()) {
-            isEnough = false;
-        }
-        return isEnough;
+        return coffee >= r.getAmtCoffee() && milk >= r.getAmtMilk() && sugar >= r.getAmtSugar()
+                && chocolate >= r.getAmtChocolate();
     }
-    
+
     /**
      * Method toString.
+     * 
      * @return String
      */
+    @Override
     public String toString() {
-    	return "Coffee: " + getCoffee() + "\n" +
-			"Milk: " + getMilk() + "\n" +
-			"Sugar: " + getSugar() + "\n" +
-			"Chocolate: " + getChocolate() + "\n";
+        return "Coffee: " + coffee + "\n" +
+                "Milk: " + milk + "\n" +
+                "Sugar: " + sugar + "\n" +
+                "Chocolate: " + chocolate + "\n";
     }
 }
